148d8564c0743610cd54380c33ca3d8e2ea44853
haruki7049 <tontonkirikiri@gmail.com>
2024-09-05 17:56:42 +0900

    style: nix fmt

--- flake.nix
+++ flake.nix
@@ -6,39 +6,39 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }:
-  {
-    templates = {
-      rust-project = {
-        path = ./templates/sbcl-project;
-        description = "A SBCL template, using treefmt-nix and flake-utils";
-        welcomeText = ''
-          # Getting started
-          - Edit project's name in flake.nix
-        '';
-      };
-    };
-  } //
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-    in
     {
-      formatter = treefmtEval.config.build.wrapper;
-
-      checks = {
-        formatting = treefmtEval.config.build.check self;
+      templates = {
+        rust-project = {
+          path = ./templates/sbcl-project;
+          description = "A SBCL template, using treefmt-nix and flake-utils";
+          welcomeText = ''
+            # Getting started
+            - Edit project's name in flake.nix
+          '';
+        };
       };
+    } //
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
 
-      devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.nil
-          pkgs.nixpkgs-fmt
-        ];
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
 
-        shellHook = ''
-          export PS1="\n[nix-shell:\w]$ "
-        '';
-      };
-    });
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nil
+            pkgs.nixpkgs-fmt
+          ];
+
+          shellHook = ''
+            export PS1="\n[nix-shell:\w]$ "
+          '';
+        };
+      });
 }
